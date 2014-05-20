0dc5fd19619697fce92b317a09ecc1d2cdfd439b
Jean Boussier <jean.boussier@gmail.com>
2014-05-20 15:08:55 -0400

    Setup travis-ci

--- spec/support/database.rb
+++ spec/support/database.rb
@@ -2,9 +2,7 @@ ActiveRecord::Base.configurations = {
   'test' => {
     adapter: 'pedant_mysql2',
     database: 'pedant_mysql2_test',
-    username: nil,
-    port: '13306',
-    host: '127.0.0.1',
+    username: 'travis',
     encoding: 'utf8',
     strict: false,
   },
