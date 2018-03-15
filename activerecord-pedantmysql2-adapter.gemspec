ad303759b975d7253a070b9bf439ffea7379b71d
Edouard CHIN <edouard.chin@shopify.com>
2018-03-15 16:57:08 -0400

    Subkeys needs to be strings

--- activerecord-pedantmysql2-adapter.gemspec
+++ activerecord-pedantmysql2-adapter.gemspec
@@ -18,7 +18,7 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ['lib']
 
-  spec.add_dependency 'activerecord', '>= 3.2'
+  spec.add_dependency 'activerecord', '>= 5.0'
   spec.add_dependency 'mysql2', '>= 0.3.12'
   spec.add_development_dependency 'bundler', '~> 1.5'
   spec.add_development_dependency 'rake'
--- spec/support/database.rb
+++ spec/support/database.rb
@@ -1,11 +1,12 @@
 ActiveRecord::Base.configurations = {
   'test' => {
-    adapter: 'pedant_mysql2',
-    database: 'pedant_mysql2_test',
-    username: 'travis',
-    encoding: 'utf8',
-    strict: false,
-    pool: 5,
-  },
+    'adapter' => 'pedant_mysql2',
+    'database' => 'pedant_mysql2_test',
+    'username' => 'travis',
+    'encoding' => 'utf8',
+    'strict' => false,
+    'pool' => 5,
+  }
 }
+
 ActiveRecord::Base.establish_connection(:test)
